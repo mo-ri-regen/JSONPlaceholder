076b87e1fd33fd32da3b823472dc50f0ed46a132
mo-ri-regen <moricontb@gmail.com>
2021-01-21 01:05:39 +0900

    PUTの追加 try,catch処理追加

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@
 
 const URL = "https://jsonplaceholder.typicode.com/posts/";
 
-const userdataButton = document.getElementById("userdata");
+const titleListButton = document.getElementById("titleList");
+const addTitleButton = document.getElementById("addTitle");
 const main = document.querySelector("main");
 
 //titleタグの作成
@@ -13,14 +14,37 @@ function CreateTitle(item) {
   return article;
 }
 
-userdataButton.addEventListener("click", async () => {
-  const res = await fetch(URL);
-  const items = await res.json();
-  items.forEach((item) => {
-    // console.log(item);
-    // console.log(item["userId"].toString());
-    console.log(item["title"]);
-    const article = CreateTitle(item);
-    main.appendChild(article);
-  });
+titleListButton.addEventListener("click", async () => {
+  try {
+    const res = await fetch(URL);
+    const items = await res.json();
+    items.forEach((item) => {
+      // console.log(item);
+      // console.log(item["userId"].toString());
+      console.log(item["title"]);
+      const article = CreateTitle(item);
+      main.appendChild(article);
+    });
+  } catch (error) {
+    alert("読み込み失敗");
+  }
+});
+
+addTitleButton.addEventListener("click", async () => {
+  try {
+    const data = {
+      title: "aa",
+      // body: 1,
+      // userId: 1,
+    };
+    const res = await fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    });
+    const post = CreateTitle(await res.json());
+    main.prepend(post); //先頭に追加
+  } catch (error) {
+    alert("データ追加に失敗");
+  }
 });
